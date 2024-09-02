9e39ddeca5ea79bd7904241f68a349d3bf1be0d4
Nidhi Tomar <nidhitomar6226@gmail.com>
2024-09-02 18:31:36 +0530

    change commit

--- Backend/index.js
+++ Backend/index.js
@@ -7,10 +7,13 @@ import userRoute from "./routes/userRoute.js";
 import companyRoute from "./routes/companyRoute.js";
 import jobRoute from "./routes/jobRoute.js";
 import applicationRoute from "./routes/applicationRoute.js";
+// import path from "path"
 
 dotenv.config({});
 
 const app = express();
+
+// const DIRNAME = path.resolve();
 
 // Middleware
 app.use(express.json());
@@ -29,6 +32,22 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
+
+// app.use(express.static(path.join(DIRNAME, "/frontend/dist")));
+// app.use("*", (_, res) =>{
+//     res.sendFile(path.resolve(DIRNAME, "frontend", "dist", "index.html"));
+// });
+
+// Connect to database and start server
+// connectDB().then(() => {
+//     const PORT = process.env.PORT || 3000;
+//     app.listen(PORT, () => {
+//         console.log(`Server running at port ${PORT}`);
+//     });
+// }).catch((error) => {
+//     console.error('Failed to connect to the database', error);
+//     process.exit(1); // Exit with a failure code if database connection fails
+// });
 
 app.listen(PORT, () => {
     connectDB();
