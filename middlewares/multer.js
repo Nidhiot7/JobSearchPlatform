369e95bd23ac2ed65c711d0c8aad8c26a7ddcf56
Nidhi Tomar <nidhitomar6226@gmail.com>
2024-09-03 09:21:52 +0530

    commit

--- middlewares/multer.js
+++
@@ -1,5 +0,0 @@
-import multer from "multer";
-
-// Multer storage configuration
-const storage = multer.memoryStorage(); // Storing file in memory, as Cloudinary will handle file storage
-export const singleUpload = multer({storage}).single("file");
