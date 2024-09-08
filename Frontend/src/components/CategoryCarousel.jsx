4f4d8762c242980e2a1f583b4494d300bc22966f
Nidhi Tomar <nidhitomar6226@gmail.com>
2024-09-08 09:12:51 +0530

    Category Carousel responsive

--- Frontend/src/components/CategoryCarousel.jsx
+++ Frontend/src/components/CategoryCarousel.jsx
@@ -14,7 +14,33 @@ const category = [
 const CategoryCarousel = () => {
     return (
         <div>
-            <Carousel className="w-full max-w-xl mx-auto my-20">
+            <Carousel className="w-full max-w-xl mx-auto my-20 hidden md:flex">
+                <CarouselContent>
+                    {
+                        category.map((cat, index) => (
+                            <CarouselItem className="md:basis-1/2 lg-basis-1/3">
+                                <Button onClick={() => searchJobHandler(cat)} variant="outline" className="rounded-full">{cat}</Button>
+                            </CarouselItem>
+                        ))
+                    }
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+
+            <div className='md:hidden lg:hidden'>
+                <MobileHeroSection />
+            </div>
+        </div>
+    )
+}
+
+export default CategoryCarousel;
+
+const MobileHeroSection = () => {
+    return (
+        <div>
+            <Carousel className="w-28 max-w-xl mx-auto my-20">
                 <CarouselContent>
                     {
                         category.map((cat, index) => (
@@ -30,7 +56,5 @@ const CategoryCarousel = () => {
         </div>
     )
 }
-
-export default CategoryCarousel
 
 
