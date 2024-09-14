cb6dd7ab0be8c52223213fa53316e124f81b9059
Nidhi Tomar <nidhitomar6226@gmail.com>
2024-09-14 09:01:55 +0530

    change

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: process.env.FRONTEND_URL,
+    origin: "http://localhost:5173",
     credentials: true
 }
 app.use(cors(corsOptions));
