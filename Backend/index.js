897ce2b65ea67515cf83f5c16ce5dc6a982e683a
Nidhi Tomar <nidhitomar6226@gmail.com>
2024-09-15 00:02:12 +0530

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -34,7 +34,7 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.use(express.static(path.join(_dirname, "frontend", "dist")));
 app.get('*', (_,res) => {
     res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
 });
