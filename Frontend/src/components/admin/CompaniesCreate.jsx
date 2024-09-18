d45cb35b039a007f07b5aae9174b94c1223b6dc2
Nidhi Tomar <nidhitomar6226@gmail.com>
2024-09-18 15:40:46 +0530

    update commit

--- Frontend/src/components/admin/CompaniesCreate.jsx
+++ Frontend/src/components/admin/CompaniesCreate.jsx
@@ -28,7 +28,7 @@ const CompanyCreate = () => {
             }
         } catch (error) {
             console.log(error);
-            toast.error(error.response?.data?.message);
+            toast.error(error.response.data.message);
         }
     }
     return (
