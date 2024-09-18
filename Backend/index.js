4802791967137abb193042d2e71927c137f6ec1e
Nidhi Tomar <nidhitomar6226@gmail.com>
2024-09-18 10:12:59 +0530

    update commit

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: "https://jobsearchplatform.onrender.com",
+    origin: "https://jobsearchplatform-frontend.onrender.com",
     credentials: true
 }
 app.use(cors(corsOptions));
